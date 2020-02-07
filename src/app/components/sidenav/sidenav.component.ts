78fbd3571a90b3750bf86eec0555480ecd318bae
-show <work.christiandecembrana@gmail.com>
2020-02-07 10:47:06 +0800

    25

--- src/app/components/sidenav/sidenav.component.ts
+++ src/app/components/sidenav/sidenav.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, transition, style, animate, state } from '@angular/animations';
 
 @Component({
   selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
-  styleUrls: ['./sidenav.component.scss']
+  styleUrls: ['./sidenav.component.scss'],
+  animations: [    
+    trigger('slideState',[
+      state('expand', style({ width: '300px' })),
+      state('collapse', style({ width: '40px' })),
+      transition( 'expand <=> collapse', animate(200) )
+    ])
+  ]
 })
 export class SidenavComponent implements OnInit {
+
+  expand = false;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  get stateName() {
+    return this.expand ? 'expand' : 'collapse';
+  }
+
+  toggle(){
+    this.expand = !this.expand;
   }
 
 }
