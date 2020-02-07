f280de6f2b5f9c24b13a119639f4c5ab7a21714b
-show <work.christiandecembrana@gmail.com>
2020-02-07 18:53:02 +0800

    aa

--- src/app/modules/animation/animations.ts
+++ src/app/modules/animation/animations.ts
@@ -3,5 +3,5 @@ import { trigger, transition, style, animate, state } from '@angular/animations'
 export let slideSidenav = trigger('slideState',[
     state('expand', style({ width: '300px' })),
     state('collapse', style({ width: '40px' })),
-    transition( 'expand <=> collapse', animate(100) )
+    transition( 'expand <=> collapse', animate(200) )
   ]);
