7205c8c2eeabddbcc9e514d8e749acdcb52eb241
-show <work.christiandecembrana@gmail.com>
2020-02-17 18:22:59 +0800

    aa

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -8,10 +8,28 @@ declare const $ : any;
 })
 export class HomeComponent implements OnInit {  
 
+  tasks = {
+    data: [
+      { id: 1, text: "Lihir", order: 10, progress: null, open: true, },
+      { id: 2, text: "Griding", progress: null, parent: 1, open: true },
+      { id: 4, text: "Mills Feeder",  duration: 8, progress: null, parent: 2, open: true,render:"split" },
+      { id: 5, text: "Mills Feeder #1", start_date: "02-03-2020", duration: 8, progress: null, parent: 4, open: true },
+      { id: 6, text: "Mills Feeder #2", start_date: "12-03-2020", duration: 3, progress: null, parent: 4, open: true },
+      { id: 7, text: "Mills Feeder #3", start_date: "22-03-2020", duration: 15, progress: null, parent: 4, open: true },
+      { id: 8, text: "NCA",  duration: 8, progress: null, parent: 2, open: true,render:"split" },
+      { id: 9, text: "NCA #1", start_date: "02-04-2020", duration: 18, progress: null, parent: 8, open: true },
+      { id: 10, text: "NCA #2", start_date: "12-02-2020", duration: 23, progress: null, parent: 8, open: true },
+      { id: 11, text: "NCA #3", start_date: "22-06-2020", duration: 5, progress: null, parent: 8, open: true },
+    ],
+    links: [
+      // {id: 1, source: 1, target: 2, type: "1"},
+      // {id: 2, source: 2, target: 3, type: "0"}
+    ]
+  };
+
   constructor() { }
 
-  ngOnInit() {
-    
+  getMarker(){
     //marker Config
     var markerId = gantt.addMarker({
       start_date: new Date(), //a Date object that sets the marker's date
@@ -19,12 +37,16 @@ export class HomeComponent implements OnInit {
       text: "Now", //the marker title
       title: ( new Date()).toDateString()
   });
+  }
 
+  getHeaders(){
     //Header Config
     gantt.config.columns =  [
       {name:"text", label:"Shutdown",  tree:true, },
     ];
+  }
 
+  getScaling(){
     //Zooming Option
     var zoomConfig = {
       levels: [
@@ -90,44 +112,40 @@ export class HomeComponent implements OnInit {
       const gantt_radio = document.querySelector(".gantt_radio[value='" +config.name+ "']" ) as HTMLInputElement;
       gantt_radio.checked  = true;
     })
-    
-    var tasks = {
-      data: [
-        {
-          id: 1, text: "Site 1", start_date: "02-02-2020", end_date: "03-04-2020",  duration: 18, order: 10,
-          progress: null, open: true
-        },
-        {
-          id: 2, text: "Plant 1", start_date: "02-02-2020", duration: 50, order: 10,
-          progress: null, parent: 1, open: true,
-        },
-        {
-          id: 3, text: "Plant 2", start_date: "15-02-2020", duration: 24, order: 20,
-          progress: null, parent: 1, open: true
-        },
-        {
-          id: 4, text: "Shutdown 1", start_date: "22-02-2020", duration: 8, order: 20,
-          progress: null, parent: 2
-        }
-      ],
-      links: [
-        // {id: 1, source: 1, target: 2, type: "1"},
-        // {id: 2, source: 2, target: 3, type: "0"}
-      ]
-    };
-  
-    gantt.init("gantt-sample");
-    gantt.config.readonly = true;
-    
-  
-    gantt.parse(tasks);    
-  
+
     var radios = document.getElementsByName("scale");
     for (var i = 0; i < radios.length; i++) {
       radios[i].onclick = function (event) {
         gantt.ext.zoom.setLevel((event.target as HTMLInputElement).value);
       };
     }
+  }
+
+  getColorCodes(){
+
+  }
+
+  initGantt( id, data ) {
+    gantt.attachEvent("onTaskClick", (id, e) => {
+      let task = gantt.getTask(id);
+
+      gantt.alert({
+        title:"Alert",
+        type:"alert-info",
+        text:"You have click on " + task.text
+      });
+    });
+    gantt.config.readonly = true;
+    gantt.init(id);    
+    gantt.parse(data);        
+  }  
+
+  ngOnInit() {               
+       
+    this.initGantt('gantt-sample', this.tasks);
+    this.getMarker();
+    this.getHeaders();
+    this.getScaling();        
   }
 
   zoomIn = () => {
